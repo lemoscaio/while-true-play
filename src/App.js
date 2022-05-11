35b760352788daeaf901b92a46065f67df5d5f60
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2022-05-11 12:04:41 -0300

    specific game page, partially static

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import Game from "./components/Game"
+
 import GlobalStyle from "./components/Normalize"
 import UserContext from "./contexts/UserContext"
-
 
 export default function App() {
     const [userInfo, setUserInfo] = React.useState({
         name: "",
         image: "",
         email: "",
-        token: ""
+        token: "",
     })
 
-    const totalInfo = {userInfo, setUserInfo};
-    
+    const totalInfo = { userInfo, setUserInfo }
+
     return (
         <UserContext.Provider value={totalInfo}>
             <BrowserRouter>
                 <GlobalStyle />
                 <Routes>
-                {/* <Route path="/" element={<Menu />} /> */}
+                    <Route path="/game/:idGame" element={<Game />} />
                 </Routes>
             </BrowserRouter>
         </UserContext.Provider>
