4f9e7835b4df3ccb207989db69761bf990da596f
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2022-05-11 13:12:20 -0300

    setting up

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react"
+import styled from "styled-components"
+import { Link, useNavigate, useParams } from "react-router-dom"
+import axios from "axios"
+import { useState, useContext, useEffect } from "react"
+import { BsCartPlus } from "react-icons/bs"
+
+import UserContext from "../contexts/UserContext"
+
+export default function Header() {
+    const { userInfo, setUserInfo } = useContext(UserContext)
+
+    return (
+        <Nav>
+            <h1>Salve</h1>
+        </Nav>
+    )
+}
+
+const Nav = styled.header``
